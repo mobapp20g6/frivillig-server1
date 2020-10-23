1334712fbda5f7d5ccc1d380ad1b2ef4a2be4ba0
Nils Jarle Haugen <nils+git@gaupne.net>
2020-10-23 16:19:45 +0200

    added scripts for common docker operations

---
+++ appsrv/flush-database.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+export COMPOSE_DOCKER_CLI_BUILD=1
+export DOCKER_BUILDKIT=1 
+export COMPOSE_DEV_FILE="docker-compose-dev.yml"
+
+echo "Stopping docker compose $COMPOSE_DEV_FILE"
+docker-compose -f $COMPOSE_DEV_FILE down --remove-orphans &&
+docker-compose -f $COMPOSE_DEV_FILE rm &&
+docker volume rm appsrv_database-data &&
+docker-compose -f $COMPOSE_DEV_FILE up -d --build database api 
+echo "The was databse and api was restarted, use CTRL-C to detach from the log console"
+sleep 1.5
+docker-compose -f $COMPOSE_DEV_FILE logs -f
