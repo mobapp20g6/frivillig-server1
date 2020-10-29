621f99e8c40a3cff4eda4fa766d73a89aa2448b8
Nils Jarle Haugen <nils+git@gaupne.net>
2020-10-29 14:25:27 +0100

    auth now ok

--- appsrv/src/main/java/no/ntnu/mobapp20g6/appsrv/config/JAXRSConfiguration.java
+++ appsrv/src/main/java/no/ntnu/mobapp20g6/appsrv/config/JAXRSConfiguration.java
@@ -9,8 +9,7 @@ import javax.ws.rs.core.Application;
  * Configures JAX-RS for the application.
  * @author Juneau
  */
-@LoginConfig(authMethod = "MP-JWT")
 @ApplicationPath("resources")
 public class JAXRSConfiguration extends Application {
-    
+
 }
