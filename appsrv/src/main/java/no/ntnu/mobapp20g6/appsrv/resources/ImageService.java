d364c7dbcf6f136e5e03401f0f27a6e7ece9739d
Martin Håhjem Årdal <martaard@stud.ntnu.no>
2020-11-02 21:42:31 +0100

    Annotate ImageService
    This commit will
    Add paths and request methods

--- appsrv/src/main/java/no/ntnu/mobapp20g6/appsrv/resources/ImageService.java
+++ appsrv/src/main/java/no/ntnu/mobapp20g6/appsrv/resources/ImageService.java
@@ -1,15 +1,25 @@
 package no.ntnu.mobapp20g6.appsrv.resources;
 
+import no.ntnu.mobapp20g6.appsrv.auth.RoleGroup;
 import no.ntnu.mobapp20g6.appsrv.dao.ImageDao;
 import no.ntnu.mobapp20g6.appsrv.dao.ImageDaoProducer;
 import no.ntnu.mobapp20g6.appsrv.model.Group;
 import no.ntnu.mobapp20g6.appsrv.model.Picture;
 import no.ntnu.mobapp20g6.appsrv.model.Task;
 import org.glassfish.jersey.media.multipart.FormDataMultiPart;
+import org.glassfish.jersey.media.multipart.FormDataParam;
 
+import javax.annotation.security.RolesAllowed;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 
-
+@Path("image")
+@Stateless
 public class ImageService {
 
     private final ImageDao dao;
@@ -18,7 +28,10 @@ public class ImageService {
         this.dao = dao;
     }
 
-    public Response getImage(Long id) {
+    @GET
+    @Path("getimage")
+    @RolesAllowed(value = {RoleGroup.USER})
+    public Response getImage(@QueryParam("imageid") Long id) {
         if (id == null)
             return Response.status(Response.Status.BAD_REQUEST).build();
         Picture image = dao.getImage(id);
@@ -27,10 +40,22 @@ public class ImageService {
         return Response.ok(image).build();
     }
 
-    public Response storeImage(Task task, Group group, FormDataMultiPart image) {
+    @POST
+    @Path("setimage")
+    @RolesAllowed(value = {RoleGroup.USER})
+    public Response storeImage(
+            @FormDataParam("task") Task task,
+            @FormDataParam("group") Group group,
+            @FormDataParam("image") FormDataMultiPart image) {
+
+        if (image == null || (task == null && group == null))
+            return Response.status(Response.Status.BAD_REQUEST).build();
         Task updatedTask = dao.storeImage(task, group, image);
         return Response.ok(updatedTask).build();
     }
+
+    @POST
+    @Path("testsetimage")
     public Response testStoreImage(
             @FormDataParam("image") FormDataMultiPart image) {
 
