dd3ab61ef954634d1a79855d59d79a1a743a8b9a
Martin Håhjem Årdal <martaard@stud.ntnu.no>
2020-11-02 17:07:57 +0100

    Add tests for setImage
    This commit will:
    Add new tests for setImage
    Restructure ImageServiceTest to have nested inner classes for each ImageServiceMethod

--- appsrv/src/test/java/no/ntnu/mobapp20g6/appsrv/resources/ImageServiceTest.java
+++ appsrv/src/test/java/no/ntnu/mobapp20g6/appsrv/resources/ImageServiceTest.java
@@ -1,64 +1,108 @@
 package no.ntnu.mobapp20g6.appsrv.resources;
 
-import no.ntnu.mobapp20g6.appsrv.dao.ImageDao;
 import no.ntnu.mobapp20g6.appsrv.dao.ImageDaoStub;
+import no.ntnu.mobapp20g6.appsrv.model.Picture;
 import no.ntnu.mobapp20g6.appsrv.model.Task;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+
 import static org.junit.Assert.*;
 import javax.ws.rs.core.Response;
 
+@RunWith(Enclosed.class)
 class ImageServiceTest {
 
-    @Test
-    public void givenGetImage_whenExistingImageIDGiven_thenResponseIsOK() {
-        ImageService imageService = new ImageService(new ImageDaoStub());
-        Response response = imageService.getImage(ImageDaoStub.EXISTING_ID);
-        assertEquals(
-                "HTTP Response should be 200",
-                Response.Status.OK.getStatusCode(),
-                response.getStatus()
-        );
+    @Nested
+    class GivenGetImage {
+        @Test
+        void whenExistingImageIDGiven_thenResponseIsOKAndContainsImage() {
+            ImageService imageService = new ImageService(new ImageDaoStub());
+            Response response = imageService.getImage(ImageDaoStub.EXISTING_ID);
+            assertEquals(
+                    "HTTP Response should be 200",
+                    Response.Status.OK.getStatusCode(),
+                    response.getStatus()
+            );
+            assertEquals(
+                    "HTTP Response should contain Image",
+                    Picture.class,
+                    response.getEntity().getClass()
+            );
+        }
+
+        @Test
+        void whenWrongImageIDGiven_thenResponseIsNotFound() {
+            ImageService imageService = new ImageService(new ImageDaoStub());
+            Response response = imageService.getImage(ImageDaoStub.NOT_EXISTING_ID);
+            assertEquals(
+                    "HTTP Response should be 404",
+                    Response.Status.NOT_FOUND.getStatusCode(),
+                    response.getStatus()
+            );
+        }
+
+        @Test
+        void whenIDisNull_thenResponseIsBadRequest() {
+            ImageService imageService = new ImageService(new ImageDaoStub());
+            Response response = imageService.getImage(ImageDaoStub.BAD_ID);
+            assertEquals(
+                    "HTTP Response should be 400",
+                    Response.Status.BAD_REQUEST.getStatusCode(),
+                    response.getStatus()
+            );
+        }
     }
 
-    @Test
-    public void givenGetImage_whenWrongImageIDGiven_thenResponseIsNotFound() {
-        ImageService imageService = new ImageService(new ImageDaoStub());
-        Response response = imageService.getImage(ImageDaoStub.NOT_EXISTING_ID);
-        assertEquals(
-                "HTTP Response should be 404",
-                Response.Status.NOT_FOUND.getStatusCode(),
-                response.getStatus()
-        );
-    }
+    @Nested
+    class GivenStoreImage {
+        @Test
+        void whenCorrectRequest_theResponseIsOKAndContainsTask() {
+            ImageService imageService = new ImageService(new ImageDaoStub());
+            Response response = imageService.storeImage(
+                    ImageDaoStub.EXISTING_TASK,
+                    ImageDaoStub.EXISTING_GROUP,
+                    ImageDaoStub.INCOMING_PHOTO);
+            assertEquals(
+                    "HTTP Response should be 200",
+                    Response.Status.OK.getStatusCode(),
+                    response.getStatus()
+            );
+            assertEquals(
+                    "HTTP Response should contain Task",
+                    Task.class,
+                    response.getEntity().getClass()
+            );
+        }
 
-    @Test
-    public void givenGetImage_whenIDisNull_thenResponseIsBadRequest() {
-        ImageService imageService = new ImageService(new ImageDaoStub());
-        Response response = imageService.getImage(ImageDaoStub.BAD_ID);
-        assertEquals(
-                "HTTP Response should be 400",
-                Response.Status.BAD_REQUEST.getStatusCode(),
-                response.getStatus()
-        );
-    }
+        @Test
+        void whenMissingImage_theResponseIsBadRequest() {
+            ImageService imageService = new ImageService(new ImageDaoStub());
+            Response response = imageService.storeImage(
+                    ImageDaoStub.EXISTING_TASK,
+                    ImageDaoStub.EXISTING_GROUP,
+                    null
+            );
+            assertEquals(
+                    "HTTP Response should be 400",
+                    Response.Status.BAD_REQUEST.getStatusCode(),
+                    response.getStatus()
+            );
+        }
 
-    @Test
-    public void givenStoreImage_whenCorrectRequest_TheResponseIsOKAndContainsTask() {
-        ImageService imageService = new ImageService(new ImageDaoStub());
-        Response response = imageService.storeImage(
-                ImageDaoStub.EXISTING_TASK,
-                ImageDaoStub.EXISTING_GROUP,
-                ImageDaoStub.INCOMING_PHOTO);
-        assertEquals(
-                "HTTP Response should be 200",
-                Response.Status.OK.getStatusCode(),
-                response.getStatus()
-        );
-        assertEquals(
-                "HTTP Response should contain Task",
-                Task.class,
-                response.getEntity().getClass()
-        );
+        @Test
+        void whenMissingBothTaskAndGroup_thenResponseIsBadRequest() {
+            ImageService imageService = new ImageService(new ImageDaoStub());
+            Response response = imageService.storeImage(
+                    null,
+                    null,
+                    ImageDaoStub.INCOMING_PHOTO
+            );
+            assertEquals(
+                    "HTTP Response should be 400",
+                    Response.Status.BAD_REQUEST.getStatusCode(),
+                    response.getStatus());
+        }
     }
-
 }
