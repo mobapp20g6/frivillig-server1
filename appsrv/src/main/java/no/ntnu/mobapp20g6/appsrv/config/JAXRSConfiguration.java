c0b9fb5efac5281407ff318f0ebecb32d4e12018
Martin Håhjem Årdal <martaard@stud.ntnu.no>
2020-11-02 21:38:00 +0100

    Add startup injection of MultiPartFeature

--- appsrv/src/main/java/no/ntnu/mobapp20g6/appsrv/config/JAXRSConfiguration.java
+++ appsrv/src/main/java/no/ntnu/mobapp20g6/appsrv/config/JAXRSConfiguration.java
@@ -1,9 +1,12 @@
 package no.ntnu.mobapp20g6.appsrv.config;
 
 import org.eclipse.microprofile.auth.LoginConfig;
+import org.glassfish.jersey.media.multipart.MultiPartFeature;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Configures JAX-RS for the application.
@@ -11,5 +14,13 @@ import javax.ws.rs.core.Application;
  */
 @ApplicationPath("resources")
 public class JAXRSConfiguration extends Application {
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        final Set<Class<?>> classes = new HashSet<Class<?>>();
+        // register resources and features
+        classes.add(MultiPartFeature.class);
+        return classes;
+    }
 
 }
