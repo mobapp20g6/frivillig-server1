9f476360781bc1396f3ca4bd2d54dbda7d8a2af3
Nils Jarle Haugen <nils+git@gaupne.net>
2020-11-05 19:59:44 +0100

    start on location DAO

---
+++ appsrv/src/main/java/no/ntnu/mobapp20g6/appsrv/dao/LocationDAO.java
@@ -0,0 +1,16 @@
+package no.ntnu.mobapp20g6.appsrv.dao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+public class LocationDAO {
+
+    @PersistenceContext
+    EntityManager em;
+
+
+
+
+
+
+}
